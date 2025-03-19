6bff9a20f0422870f0f8c76dd4155654332f1e54
Cooper <coopdav4@gmail.com>
2025-03-19 09:12:46 -0600

    just making sure everything is up to date

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import InputDesign from './components/InputDesign.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import App from "./App"; // Use App.tsx instead of InputDesign.tsx
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <InputDesign />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
