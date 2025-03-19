a80d4f06ba57e532f142294180d66470fe0538be
williaal <lailai26williams@gmail.com>
2025-03-19 09:20:34 -0600

    Created Standardized BottomNavBar

---
+++ frontend/src/components/InputDesignExample.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import BottomNavigation from './BottomNavigation';
+
+const InputDesign: React.FC = () => {
+  return (
+    <>
+      <div>
+        <BottomNavigation />
+      </div>
+    </>
+  );
+};
+
+export default InputDesign;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import InputDesign from './components/InputDesign.tsx';
+import InputDesign from './components/InputDesignExample.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
