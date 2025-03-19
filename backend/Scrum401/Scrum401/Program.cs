64cc9db255f669cfc943e7159a2e1bc3786e1173
uboston <uboston@byu.edu>
2025-03-19 08:50:48 -0600

    BostonSeanBenjiJohn

--- backend/Scrum401/Scrum401/Program.cs
+++ backend/Scrum401/Scrum401/Program.cs
@@ -1,11 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Scrum401.Data;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<YSAContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("YSAConnection")));
 
 var app = builder.Build();
 
