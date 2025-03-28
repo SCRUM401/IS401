90e400cd5d1fc85ac73b9a1176d0ba8b75a07d62
ccloud8939 <cclo8939@byu.edu>
2025-03-27 18:33:15 -0600

    commit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import EventList from './components/EventList.tsx';
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
-import App from "./App"; // Use App.tsx instead of InputDesign.tsx
-import "./index.css";
->>>>>>> 7c3e732616e2a2bac02f332c59fcab71c3e45267
+import App from './App.tsx';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
-    <EventList />
-=======
       <App />
->>>>>>> 7c3e732616e2a2bac02f332c59fcab71c3e45267
   </StrictMode>
 );
